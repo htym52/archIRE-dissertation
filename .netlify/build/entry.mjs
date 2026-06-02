caf5ed0ba392b0b8ea74c9ff8e8702902269792e
htym52 <ilihtym52@gmail.com>
2026-06-02 21:26:51 +0100

    UPDATE-F

--- .netlify/build/entry.mjs
+++
@@ -1,1 +0,0 @@
-export { l as createHandler } from './chunks/ssr-function_CjI6CjEc.mjs';
--- .netlify/v1/functions/ssr/.netlify/build/chunks/supabase_BflDeDat.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://vgyslbcclavibzjngpfv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZneXNsYmNjbGF2aWJ6am5ncGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNzAzOTQsImV4cCI6MjA5NDk0NjM5NH0.W-fvSNIuxZ7u6wrFMksUZm9HeiyQPa05PYufBHto9zo"
-);
-
-export { supabase as s };
